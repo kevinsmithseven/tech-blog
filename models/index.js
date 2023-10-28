c837faacce46176c196dc83599adb5fa8242576d
kevinsmithseven <kevinsmithseven@gmail.com>
2023-10-28 16:44:18 -0500

    updated models/index.js with associations for User, Blogpost and Comment models

--- models/index.js
+++ models/index.js
@@ -1,3 +1,35 @@
 const User = require('./User');
 const Blogpost = require('./Blogpost');
 const Comment = require('./Comment');
+
+User.hasMany(Blogpost, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL',
+});
+
+Blogpost.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL',
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL',
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL',
+});
+
+Blogpost.hasMany(Comment, {
+    foreignKey: 'blogpost_id',
+    onDelete: 'SET NULL',
+});
+
+Comment.belongsTo(Blogpost, {
+    foreignKey: 'blogpost_id',
+    onDelete: 'SET NULL',
+});
+
+module.exports = { User, Blogpost, Comment };
