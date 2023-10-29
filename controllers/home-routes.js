b7b6136eb391476b2014c2542afbf6528289f14f
kevinsmithseven <kevinsmithseven@gmail.com>
2023-10-29 14:35:34 -0500

    created signup handlebars view, created home-routes.js in controllers and added GET route for blogposts

---
+++ controllers/home-routes.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+const { Blogpost } = require('./models');
+
+// GET all blogposts for homepage
+router.get('/', async (req, res) => {
+    try {
+        const blogpostData = await Blogpost.findAll({
+        });
+
+        const blogposts = blogpostData.map((blogpost) =>
+            blogpost.get({ plain: true })
+        );
+
+        res.render('homepage', {
+            blogposts,
+            loggedIn: req.session.loggedIn,
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
