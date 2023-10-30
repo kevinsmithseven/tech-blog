b476e5e32b8c35950acc082e86b88e7764d52abb
kevinsmithseven <kevinsmithseven@gmail.com>
2023-10-30 16:25:52 -0500

    added connect-sessions package and required on server.js, updated html for homepage.handlebars

--- server.js
+++ server.js
@@ -4,24 +4,28 @@ const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
-
-// TODO Placeholder for session store
-// const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// TODO Placeholder for sessions
-// const sess = {
-//     secret: 'Super secret secret',
-//     cookie: {},
-//     resave: false,
-//     saveUninitialized: true,
-//     store: new SequelizeStore({
-//       db: sequelize
-//     })
-//   };
-// app.use(session(sess));
+
+const sess = {
+    secret: process.env.SECRET,
+    cookie: {
+        maxAge: 60 * 60 * 1000,
+        httpOnly: true,
+        secure: false,
+        sameSite: 'strict',
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize
+    })
+  };
+
+app.use(session(sess));
 
 const hbs = exphbs.create({});
 
