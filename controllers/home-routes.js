78e1d6f08db302391c84d32011306085ae362978
kevinsmithseven <kevinsmithseven@gmail.com>
2023-10-31 10:00:55 -0500

    added GET route for login view

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -55,6 +55,15 @@ router.get('/singlepost/:id', async (req, res) => {
 
 
 // TODO GET login route
+router.get('/login', (req, res) => {
+    // If the user is already logged in, redirect the request to another route
+    if (req.session.logged_in) {
+      res.redirect('/dashboard');
+      return;
+    }
+  
+    res.render('login');
+  });
 
 
 
