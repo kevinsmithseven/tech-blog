719f22dcb079ad320f7e2c16c73d2fee432d905d
kevinsmithseven <kevinsmithseven@gmail.com>
2023-11-01 19:27:37 -0500

    multiple changes to implement post creation, added update and delete routes, updated styling

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,7 +2,9 @@ const router = require('express').Router();
 const userRoutes = require('./user-routes');
 const singlepostRoutes = require('./blogpost-routes')
 
+
 router.use('/users', userRoutes);
 router.use('/blogpost', singlepostRoutes);
+
 
 module.exports = router;
